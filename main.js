cf969bfe9fe9215db73a71ad2aede5580c5b2621
Paper Lee <paper.li@gmail.com>
2013-09-26 03:02:00 +0800

    About section basic schema

---
+++ main.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+	
+	$("#bg_about").load(function(){
+		stackBlurImage("bg_about","canvas_about",24,true,[parseInt($("#about>.description").position().left),parseInt($("#about>.description").position().top),parseInt($("#about>.description").width()),parseInt($("#about>.description").height())]);
+		console.log("x: "+$("#about>.description").position().left);
+		console.log("y: "+$("#about>.description").position().top);
+		console.log("w: "+$("#about>.description").width());
+		console.log("h: "+$("#about>.description").height());
+		//$(this).hide();
+		$("#about>.description_bg").height($("#about>.description").outerHeight());
+		//$("#about>.description_bg").css("left",$("#about>.description").position().left);
+		$("#about>.description_bg").css("top",$("#about>.description").position().top);
+		$("#about>.description_bg>canvas").css("top",-1*$("#about>.description").position().top);
+	});
+	
+	$(document).scroll(function(){
+		//console.log($(document).scrollTop());
+		
+		var d1 = $(document).height() - $(window).height();
+		var d2 = $("#bg_about").height()-$("#about>.description_bg").height();
+		$("#about>.description").css("top",$(document).scrollTop()*(d2/d1));
+		console.log($(document).height());
+		console.log($(window).height());
+		console.log(d2);
+		console.log(d1);
+		console.log($(document).scrollTop()*(d2/d1));
+		//reset mask position
+		$("#about>.description_bg").css("top",$("#about>.description").position().top);
+		$("#about>.description_bg>canvas").css("top",-1*$("#about>.description").position().top);
+	});
+	/*var c = document.getElementById("canvas_about");
+	var ctx = c.getContext("2d");
+	ctx.rect();
+	console.log("x: "+$("#about>.description").position().left);
+	console.log("y: "+$("#about>.description").position().top);
+	console.log("w: "+$("#about>.description").width());
+	console.log("h: "+$("#about>.description").height());
+	ctx.clip();*/
+});
