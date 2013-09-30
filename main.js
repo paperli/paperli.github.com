0f2cf4d3f50d954e62e7489794ccc3ea3340b1b0
Paper Lee <paper.li@gmail.com>
2013-10-01 02:01:07 +0800

    Done the many pics showcase. Todo: refine the animation between

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var cases_data = [{
 		title:'Pilotfish MCBW event',
 		article:'<p><a href=\'http://www.pilotfish.eu/mcbw/\' target=\'_blank\'>http://www.pilotfish.eu/mcbw/</a></p><p>The big innovation event in Munich! Munich Creative Business Week. I designed a webpage to present speech schedule in Pilotfish booth. <b>It\'s all about time!</b> The main part of this design is the time-line and the blobs of speakers. As requesed also made the pdf version and invitation card.</p>',
 		//article:'The big innovation event in Munich! Munich Creative Business Week. I designed a webpage to present speech schedule in Pilotfish booth. The main part of this design is the time-line and the blobs of speakers. As requesed also made the pdf version and invitation card.',
-		bg_url:'images/mcbw1.png',
+		bg_url:['images/mcbw1.png','images/mcbw2.png','images/mcbw4.png'],
 		img_id:'bg_mcbw',
 		canvas_id:'canvas_mcbw'
 	},
@@ -10,7 +10,7 @@ var cases_data = [{
 			title:'MobiApps Website',
 			article:'<p><a href=\'http://www.themobiapps.com\' target=\'_blank\'>http://www.themobiapps.com/</a></p><p>TheMobiApps, the mobile app start-up in Taiwan, was planning to build the company identify prior to media publishing. I designed the whole new website with the 4 key colors in the company logo. The website consisted of the touch keys in present design trend, one-page scroll, simple and html5. It is planned to publish in March, 2013.</p>',
 			//article:'The big innovation event in Munich! Munich Creative Business Week. I designed a webpage to present speech schedule in Pilotfish booth. The main part of this design is the time-line and the blobs of speakers. As requesed also made the pdf version and invitation card.',
-			bg_url:'images/mobi3.png',
+			bg_url:['images/mobi1.png','images/mobi2.png','images/mobi3.png'],
 			img_id:'bg_mobiapps',
 			canvas_id:'canvas_mobiapps'
 		},
@@ -18,16 +18,21 @@ var cases_data = [{
 				title:'MobiApps',
 				article:'<p><a href=\'https://itunes.apple.com/tw/app/ding-hao/id577065907?mt=8\' target=\'_blank\'>Mobiapps @App Store</a></p><p>In end of 2012, I joined the joyful team MobiApps to lead the UX and UI design. We server the mobile app for retail sale and small business. With rapid design pattern, the mobile app can be prompt built and delivered. The flexibility of the team allowed me to break out serveral concepts including smart no-content mode, social biz UI, etc.. Still, I used variable tools like Dropbox or TAP to rapidly test new concept.</p>',
 				//article:'The big innovation event in Munich! Munich Creative Business Week. I designed a webpage to present speech schedule in Pilotfish booth. The main part of this design is the time-line and the blobs of speakers. As requesed also made the pdf version and invitation card.',
-				bg_url:'images/mobiapps2.png',
+				bg_url:['images/mobiapps1.png','images/mobiapps2.png','images/mobiapps3.png','images/mobiapps4.png'],
 				img_id:'bg_mobiapps_app',
 				canvas_id:'canvas_mobiapps_app'
 			}];
 
-			var BLUR_RADIUS = 32;
+var BLUR_RADIUS = 32;
 
+//window width: pic is 100% width of window
+var pic_width;
 
+var at_pos = [];
 
 $(document).ready(function(){
+	
+	pic_width = $(window).width();
 	
 	$("#bg_about").load(function(){
 		stackBlurImage("bg_about","canvas_about",BLUR_RADIUS,true,[parseInt($("#about>.description").position().left),parseInt($("#about>.description").position().top),parseInt($("#about>.description").width()),parseInt($("#about>.description").height())]);
@@ -69,12 +74,12 @@ $(document).ready(function(){
 			
 			//hide/appear button margin: top:20 left:48
 			var btn_top = parseInt($("div.case-section:nth-child("+(parseInt(key)+1)+")>div.case-description").css("top"))+20;
-			console.log("button top@"+key+" shall be "+btn_top);
+			//console.log("button top@"+key+" shall be "+btn_top);
 			$("div.case-section:nth-child("+(parseInt(key)+1)+")>a.btnAppear").css("top",btn_top).css("right",48);
 			
-			console.log("top@"+key+" is "+$("div.case-section:nth-child("+(parseInt(key)+1)+")>div.case-description").css("top"));
+			//console.log("top@"+key+" is "+$("div.case-section:nth-child("+(parseInt(key)+1)+")>div.case-description").css("top"));
 			//console.log($("div.case-section:nth-child("+(parseInt(key)+1)+")>a.btnAppear").html());
-													
+												
 		}
 		
 	});
@@ -84,25 +89,40 @@ $(document).ready(function(){
 	for (var key in cases_data){
 		$("#cases").loadTemplate($("#template"),cases_data[key],{append:true});
 		//$("#cases").append(case_box);
+		
+		at_pos.push(0);
 	}
 	
 	$("#cases>div.case-section").each(function(key,element){
+
+		//assign pics
+		var pics = cases_data[key]["bg_url"];
+		var id_base = cases_data[key]["img_id"];
+		var pic;
+		for (var i in pics){
+			pic = $("<img />").addClass("case-bg").attr("width","100%").attr("id",id_base+i).attr("src",pics[i]).css("left",(i*100)+"%");
+			$(element).children("div.img-box").append(pic);
+		}
+		
+		
+		
 		//blur the description bg
 		var img_id, canvas_id;
-		$(element).children("img.case-bg").load(function(){
+		$(element).find("img.case-bg:nth-child(1)").load(function(){
 			img_id = $(this).attr("id");
 			//console.log($(this).parent().find("canvas.case-blur_canvas").html());
-			canvas_id = $(this).parent().find("canvas.case-blur_canvas").attr("id");
+			canvas_id = $(this).parent().parent().find("canvas.case-blur_canvas").attr("id");
 			//console.log(img_id+"::"+canvas_id);
 			stackBlurImage(img_id,canvas_id,BLUR_RADIUS,true);
-			$(this).parent().children("div.case-description_bg").height($(this).parent().children("div.case-description").outerHeight());
+			$(this).parent().parent().children("div.case-description_bg").height($(this).parent().parent().children("div.case-description").outerHeight());
 			//$("#about>.description_bg").css("left",$("#about>.description").position().left);
 			//console.log("top:"+$(this).parent().children("div.case-description").position().top);
-			$(this).parent().children("div.case-description_bg").css("top",$(this).parent().children("div.case-description").position().top);
-			$(this).parent().find("canvas.case-blur_canvas").css("top",-1*$(this).parent().children("div.case-description").position().top);
+			$(this).parent().parent().children("div.case-description_bg").css("top",$(this).parent().parent().children("div.case-description").position().top);
+			$(this).parent().parent().find("canvas.case-blur_canvas").css("top",-1*$(this).parent().parent().children("div.case-description").position().top);
 		});
 		
 		$(element).find("a.btnHide").click(function(){
+			
 			$(this).parent().parent().children("div.case-description_bg").fadeOut();
 			$(this).parent().fadeOut();
 			
@@ -115,6 +135,41 @@ $(document).ready(function(){
 			$(this).parent().children("div.case-description").fadeIn();
 			$(this).parent().children("div.case-description_bg").fadeIn();
 		});
+		
+		//Register prev. and next. button actions
+		$(element).find("a.btnNext").click(function(){
+			//next pic
+			//console.log($(this).parent().children("div.img-box"));
+			//$(this).parent().children("div.img-box").scrollLeft($(this).parent().children("div.img-box").scrollLeft()+$(this).parent().children("div.img-box").width());
+			$(this).parent().children("div.img-box").animate({scrollLeft:"+="+$(this).parent().children("div.img-box").width()});
+			at_pos[key]+=1;
+			if (at_pos[key] >= cases_data[key]["bg_url"].length){
+				at_pos[key] = cases_data[key]["bg_url"].length-1;
+			} else {
+				//have to re-blur new pic
+				img_id = $(element).find("img.case-bg:nth-child("+(at_pos[key]+1)+")").attr("id");
+				canvas_id = $(element).find("canvas.case-blur_canvas").attr("id");
+				$("#"+canvas_id).fadeOut("fast").fadeIn("fast");
+				stackBlurImage(img_id,canvas_id,BLUR_RADIUS,true);
+			}
+			
+		});
+		
+		$(element).find("a.btnPrev").click(function(){
+			//prev pic
+			//$(this).parent().children("div.img-box").scrollLeft($(this).parent().children("div.img-box").scrollLeft()-$(this).parent().children("div.img-box").width());
+			$(this).parent().children("div.img-box").animate({scrollLeft:"-="+$(this).parent().children("div.img-box").width()});
+			at_pos[key]-=1;
+			if (at_pos[key] < 0){
+				at_pos[key] = 0;
+			} else {
+				//have to re-blur new pic
+				img_id = $(element).find("img.case-bg:nth-child("+(at_pos[key]+1)+")").attr("id");
+				canvas_id = $(element).find("canvas.case-blur_canvas").attr("id");
+				$("#"+canvas_id).fadeOut("fast").fadeIn("fast");
+				stackBlurImage(img_id,canvas_id,BLUR_RADIUS,true);
+			}
+		});
 	});
 	
 	/*$("#cases").loadTemplate($("#template"),{
@@ -126,5 +181,7 @@ $(document).ready(function(){
 	});*/
 	
 	//$("div.case-section").children("img.case-bg").each()
+	
+	
 	
 });
